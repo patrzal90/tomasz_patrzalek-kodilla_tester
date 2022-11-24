f171e3dfc0065769e13e3ea76447d5f8238eca3a
Tomasz Patrzalek <patrzal90@gmail.com>
2022-11-24 22:32:49 +0000

    hello world

--- kodilla_intro/src/main/java/FirstClass.java
+++ kodilla_intro/src/main/java/FirstClass.java
@@ -1,6 +1,4 @@
-package com.kodilla;
-
-public class Main {                           // [1]
+public class FirstClass {                           // [1]
     public static void main(String[] args) {         // [2]
         System.out.println("Hello from FirstClass!"); // [3]
     }
